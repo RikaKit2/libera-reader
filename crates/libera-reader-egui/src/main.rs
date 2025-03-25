4b7895a0aa6b5a7ab65be23e6a80dd76e6ef4c85
RikaKit2 <174120712+RikaKit2@users.noreply.github.com>
2025-03-25 19:06:23 +0400

    fixed a bug in the side_bar in libera-reader-egui crate

--- crates/libera-reader-egui/src/main.rs
+++ crates/libera-reader-egui/src/main.rs
@@ -1,19 +1,23 @@
+use crate::router::{BaseRoute, RootRoute, Router};
 use eframe::{run_native, Frame, NativeOptions};
 use egui::Context;
 use egui_extras::install_image_loaders;
 use ui::pages::main::side_bar;
 
+
 mod router;
 mod ui;
-mod glob;
 
 pub struct App {
   side_bar: side_bar::State,
+  router: Router,
 }
 
 impl App {
   pub fn new() -> Self {
-    Self { side_bar: side_bar::State::new() }
+    let router = Router::new(RootRoute::Base(BaseRoute::Library));
+    let side_bar = side_bar::State::new();
+    Self { side_bar, router }
   }
 }
 
