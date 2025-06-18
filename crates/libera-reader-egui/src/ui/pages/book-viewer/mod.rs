7618f82b145cfa9586c8321bacf00f435394e689
RikaKit2 <174120712+RikaKit2@users.noreply.github.com>
2025-06-18 10:50:43 +0400

    Removed egui and slint implementation

--- crates/libera-reader-egui/src/ui/pages/book-viewer/mod.rs
+++
@@ -1,9 +0,0 @@
-use egui::Context;
-
-pub(crate) struct BookViewer {}
-impl BookViewer {
-  pub(crate) fn new() -> Self {
-    Self {}
-  }
-  pub(crate) fn make(&mut self, ctx: &Context) {}
-}
--- crates/libera-reader-slint/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-  slint_build::compile("ui/root.slint").expect("Slint build failed");
-}
