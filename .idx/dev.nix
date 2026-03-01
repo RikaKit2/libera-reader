424d6d24367c40417db10622aced621a65f14631
RikaKit2 <174120712+RikaKit2@users.noreply.github.com>
2026-03-01 17:07:37 +0400

    Fix app crash

---
+++ .idx/dev.nix
@@ -0,0 +1,75 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "unstable"; # or "stable-24.05"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+    pkgs.clang
+    pkgs.wild
+    pkgs.openssl
+    pkgs.fish
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.neovim
+    pkgs.gcc
+    pkgs.pkg-config
+    pkgs.alsa-lib
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXi
+    pkgs.xorg.libXinerama
+    pkgs.xorg.libxcb
+    pkgs.wayland
+    pkgs.vulkan-loader
+    pkgs.sccache
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "asvetliakov.vscode-neovim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
--- crates/libera-reader/src/main.rs
+++ crates/libera-reader/src/main.rs
@@ -5,16 +5,20 @@ mod ui;
 use crate::ui::pages::Pages;
 use crate::{theme::init_theme, ui::assets::Assets};
 use anyhow::Result;
+use gpui::AppContext;
 use gpui::{App, Application, Bounds, Entity, TitlebarOptions, Window, WindowBounds, WindowOptions, px, size};
+use gpui_component::Root;
 use libera_reader_core::ctx::Ctx;
 use mimalloc::MiMalloc;
 use std::path::PathBuf;
 use utils::{create_subscriber, debug};
+
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-fn build_root_window(window: &mut Window, cx: &mut App) -> Entity<Pages> {
-  Pages::new(window, cx)
+fn build_root_window(window: &mut Window, cx: &mut App) -> Entity<Root> {
+  let pages = Pages::new(window, cx);
+  cx.new(|cx| Root::new(pages, window, cx))
 }
 
 fn main() -> Result<()> {
@@ -31,12 +35,14 @@ fn main() -> Result<()> {
     Ctx::init(cx);
     gpui_component::init(cx);
     init_theme(themes_dir, cx);
+
     let bounds = Bounds::centered(None, size(px(700.0), px(400.0)), cx);
     let window_options = WindowOptions {
       window_bounds: Some(WindowBounds::Windowed(bounds)),
       titlebar: Some(TitlebarOptions { title: Some("Libera Reader".into()), ..Default::default() }),
       ..Default::default()
     };
+
     cx.open_window(window_options, build_root_window).unwrap();
   });
   Ok(())
