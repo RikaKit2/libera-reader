49357f41b92512fcf316158ea484b1c0c226bf0e
RikaKit2 <174120712+RikaKit2@users.noreply.github.com>
2026-08-09 15:55:58 +0400

    update flake.nix, fix vulnerability

--- flake.nix
+++ flake.nix
@@ -5,23 +5,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    libs = with pkgs; [
-      wayland vulkan-loader libxkbcommon fontconfig alsa-lib
-      libX11 libXcursor libXrandr libXi libXinerama libxcb gtk3
-      glib
-      gdk-pixbuf zenity
-    ];
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [ pkg-config clang rustup ];
-      buildInputs = libs;
-      shellHook = ''
-        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
-        export WINIT_UNIX_BACKEND=wayland
-      '';
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      libs = with pkgs; [
+        wayland
+        vulkan-loader
+        libxkbcommon
+        fontconfig
+        freetype
+        openssl
+        alsa-lib
+        libX11
+        libXcursor
+        libXrandr
+        libXi
+        libXinerama
+        libxcb
+        gtk3
+        glib
+        gdk-pixbuf
+        zenity
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          clang
+          rustup
+        ];
+        buildInputs = libs;
+        shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
+          export PKG_CONFIG_PATH="${pkgs.fontconfig.dev}/lib/pkgconfig:${pkgs.freetype.dev}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
+          export WINIT_UNIX_BACKEND=wayland
+        '';
+      };
     };
-  };
 }
